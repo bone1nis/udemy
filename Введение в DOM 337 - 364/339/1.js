49cc91e9c9b1f94693bc3de006bd15972469ad4a
bone1nis <bone1nis@gmail.com>
2024-11-22 08:46:50 +0300

    feat2

---
+++ Введение в DOM 337 - 364/339/1.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let text1 = document.querySelector("#elem1");
+let text2 = document.querySelector("#elem2");
+let text3 = document.querySelector("#elem3");
+
+console.log(text1);
+console.log(text2);
+console.log(text3);
